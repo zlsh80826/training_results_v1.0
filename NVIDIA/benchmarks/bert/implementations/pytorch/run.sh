c55ced1998e4b68549bd1ad7f38b05ea3bbb7878
zlsh80826 <zlsh80826@gmail.com>
2021-07-30 02:55:19 -0700

    update ckpt path

--- NVIDIA/benchmarks/bert/implementations/pytorch/run.sh
+++ NVIDIA/benchmarks/bert/implementations/pytorch/run.sh
@@ -6,7 +6,7 @@ CONT=${USER}/mlperf-v1.0-nvidia:language_model \
 DATADIR=$PTY_DATA_DIR \
 DATADIR_PHASE2=$PTY_DATA_DIR \
 EVALDIR=$PWD/bert_data/eval_set_uncompressed \
-CHECKPOINTDIR=$PWD/ckpt \
-CHECKPOINTDIR_PHASE1=$PWD/ckpt \
+CHECKPOINTDIR=$PWD/bert_data \
+CHECKPOINTDIR_PHASE1=$PWD/bert_data \
 ./run_with_docker.sh
 done
